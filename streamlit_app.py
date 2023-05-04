0f3a44a1d7e77b155fe993a5fed43ccd898fa98f
KoushikSingha1994 <132549490+KoushikSingha1994@users.noreply.github.com>
2023-05-04 13:44:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.header('Hello Folks')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'])
-if fruit_selected is none
+if fruit_selected is None
   fruit_to_show = my_fruit_list
 else
   fruit_to_show = my_fruit_list.loc[fruit_selected]
