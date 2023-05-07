3589ecba5c80786490d3d8bac8e20bac14f945e4
KoushikSingha1994 <132549490+KoushikSingha1994@users.noreply.github.com>
2023-05-07 12:05:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,10 @@ streamlit.header('Hello Folks')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-  if not fruit_selected:
-    fruit_to_show = my_fruit_list
-  else:
-    fruit_to_show = my_fruit_list.loc[fruit_selected]
-
+if not fruit_selected:
+  fruit_to_show = my_fruit_list
+else:
+  fruit_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show)
 
 streamlit.header('Frityvice Fruit Advice')
